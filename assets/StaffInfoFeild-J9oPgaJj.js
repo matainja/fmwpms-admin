24c93a6a07d7e819d093bad0e0ac59eb7eb93f0c
biswarup96 <biswarana96.rana@gmail.com>
2025-10-28 12:58:13 +0530

    dashboard has been updated and download button added here

--- assets/StaffInfoFeild-J9oPgaJj.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Dles2GGc.js";import{R as i}from"./index-D40Cld0t.js";const l=({title:a,value:r,convertValue:t,customClassName:e,customTitleClassName:m,customValueClassName:o})=>s.jsxs("div",{className:e,children:[s.jsx("span",{className:m,children:a}),s.jsx("p",{className:`text-gray-700 dark:text-gray-200 font-semibold ${o}`,children:t?i(r||""):r})]});export{l as S};
--- assets/useThemeClass-Bv4J3TKz.js
+++
@@ -1,1 +0,0 @@
-import{bM as t}from"./index-Dles2GGc.js";function n(){const{themeColor:r,primaryColorLevel:o}=t(),e=`${r}-${o}`;return{ringTheme:`ring-${e}`,borderTheme:`border-${e}`,bgTheme:`bg-${e}`,textTheme:`text-${e}`}}export{n as u};
